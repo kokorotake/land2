600db0701090ce31955a3bd22be35331c9c3d7c1
kokoro <c225213@ed.nagoya-cu.ac.jp>
2022-08-15 18:34:44 +0900

    first commit

---
+++ coco-template/index.js
@@ -0,0 +1,91 @@
+
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 3,//スライドを画面に3枚見せる
+      slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
+$('.slider2').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: false,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 3,//スライドを画面に3枚見せる
+      slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から100pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if(scroll > 0){
+		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        });	
+	}
+    return false;//リンク自体の無効化
+});
+
